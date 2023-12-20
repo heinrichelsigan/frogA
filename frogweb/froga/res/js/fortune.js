f2107254e2dd43df6bd0c895944c6ae6163549b5
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2023-12-20 07:20:56 +0100

    2023-12-20 added fortune & od cgi skeletons

---
+++ frogweb/froga/res/js/fortune.js
@@ -0,0 +1,23 @@
+﻿/*
+    2023-12-22 fortune.js © by Heinrich Elsigan
+    https://darkstar.work/js/fortune.js
+    https://area23.at/js/fortune.js
+*/
+
+var theForm;
+
+function ReloadForm() { // var url = "https://darkstar.work/cgi/fortune.cgi";
+    var delay = 16000;
+    setTimeout(function () { window.location.reload(); }, delay);
+    return;
+}
+
+function GetForm() {
+    if (document.getElementById) { theForm = document.getElementById('form1'); }
+    else { theForm = document.form1; }
+}
+
+function FormSubmit() {
+    GetForm();
+    theForm.submit();
+}
---
+++ frogweb/froga/res/js/od.js
@@ -0,0 +1,34 @@
+﻿/*
+    2023-12-20 od.js © by Heinrich Elsigan
+    https://darkstar.work/cgi/od.cgi
+    https://area23.at/test/cgi/od.html
+*/
+
+var theForm;
+
+function GetForm() {
+    if (document.getElementById) { theForm = document.getElementById('form1'); }
+    else { theForm = document.form1; }
+}
+
+function WebForm_TextBoxKeyHandler(event) {
+    var target = event.target;
+    if ((target == null) || (typeof (target) == "undefined"))
+        target = event.srcElement;
+    if (event.keyCode == 13) {
+        if ((typeof (target) != "undefined") && (target != null)) {
+            if (typeof (target.onchange) != "undefined") {
+                target.onchange();
+                event.cancelBubble = true;
+                if (event.stopPropagation) event.stopPropagation();
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function FormSubmit() {
+    GetForm();
+    theForm.submit();
+}
